f7e01347f6865a1246e5b0a3185d55f2e551deb2
Saular Raffi <fo6197@wayne.edu>
2020-01-22 14:08:32 -0500

    FreepScraper object can be passed a url and retrieve the article title and body paragraphs

--- detroit free press parser.py
+++ detroit free press parser.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup as soup
 class FreepScraper:
 	def __init__(self, url):
 		self.articleURL = url 
+		self.articleTitle = ""
+		self.articleBody = []
 
 	def setArticleTitle(self, articleTitle):
 		self.articleTitle = articleTitle
@@ -26,8 +28,16 @@ class FreepScraper:
 		return self.articleBody
 
 	def scrape(self):
-		self.page = requests.get(self.articleURL)
-		soup_page = soup(self.page.content, 'html.parser')
+		page = requests.get(self.articleURL)
+		soup_page = soup(page.content, 'html.parser')
+
+		self.articleTitle = soup_page.find_all(class_="util-bar-share-summary-title")[0].get_text()
+
+		self.articleBody = soup_page.find_all(class_="p-text")
+
+		for body in self.articleBody:
+			print(body.get_text())
+			print("\n")
 
 	def getScraped(self):
 		return soup(self.page.content, 'html.parser')
@@ -37,7 +47,7 @@ test = FreepScraper("https://www.freep.com/story/news/local/michigan/" +
 	"detroit/2019/12/05/detroit-bulk-storage-revere-copper-detroit-river-uranium/2618868001/")
 
 test.scrape()
-print(test.getScraped())
+# print(test.getScraped())
 
 # page = requests.get("https://www.freep.com/story/news/local/michigan/" + 
 # 	"detroit/2019/12/05/detroit-bulk-storage-revere-copper-detroit-river-uranium/2618868001/")
@@ -45,6 +55,8 @@ print(test.getScraped())
 # soup_page = soup(page.content, 'html.parser')
 
 # print(soup_page.prettify()) 
+
+
 
 
 
