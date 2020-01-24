eed256eb48e88b36629e3fc9ef479da7a7692497
Saular Raffi <fo6197@wayne.edu>
2020-01-24 16:15:49 -0500

    added methods to FreepCrawler to crawl and scrape.  crawlURL() crawls and stores urls for each search page, defined by keywords.  scrapeURLs() scrapes each url and stores FreepScraper objects

--- detroit free press parser.py
+++ detroit free press parser.py
@@ -4,73 +4,90 @@ import requests
 from bs4 import BeautifulSoup as soup
 
 class FreepCrawler():
+
+	# initialize variables and create list of base urls with the different search keywords
 	def __init__(self, *keywords):
 		self.urls = []
 		self.baseURLs = []
 		self.keywords = []
+		self.scrapedArticles = []
 
 		for key in keywords:
 			self.keywords.append(key)
 			self.baseURLs.append("https://www.freep.com/search/" + key + "/")
 
+	# print all urls that have been crawled
 	def printURLs(self):
-		for url in self.baseURLs:
+		for url in self.urls:
 			print(url)
 
-	def getURLs(self):
+	# for each base url, crawl all article links contained in each.  For instance, base url is the search result for polution,
+	# so crawlURLs() will retrieve article urls from that page and append them to the urls list
+	def crawlURLs(self):
 		try:
 			for url in self.baseURLs:
 				page = requests.get(url)
 				soup_page = soup(page.content, 'html.parser')
-				hrefs = soup_page.find_all('a', href=True)
+				links = soup_page.find_all('a', href=True)
 				
-				print(soup_page)
+				for link in links:
+					if "/story/news/local/michigan/" in link['href']:
+						self.urls.append("https://www.freep/" + link['href'])
 
 		except requests.exceptions.ConnectionError:
 			print("connection refused: too man requests")
 
-			# for href in hrefs:
-			# 	# if "/story/news/local/michigan" in href['href']:
-			# 	print(href['href'])
-			# 	print("\n")
+	# for each url in the urls list, scrape its content and store in scrapedArticles list
+	def scrapeURLs(self):
+		for url in self.urls:
+			article = FreepScraper(url)
+			self.scrapedArticles.append(article.scrape())
 
+	def getURLs(self):
 		return self.urls
 
-
 class FreepScraper:
+
+	# initialize variables
 	def __init__(self, url):
 		self.articleURL = url 
 		self.articleTitle = ""
 		self.articleBody = []
 
+	# set the article title to articleTitle param
 	def setArticleTitle(self, articleTitle):
 		self.articleTitle = articleTitle
 
+	# set the article url to url param
 	def setURL(self, url):
 		self.articleURL = url
 
+	# set the article body to body param
 	def setArticleBody(self, body):
 		self.articleBody = body
 
+	# return the article title
 	def getArticleTitle(self):
 		return self.articleTitle
 
+	# return the article url
 	def getArticleURL(self):
 		return self.articleURL
 
+	# return the article body
 	def getArticleBody(self):
 		return self.articleBody
 
+	# print the article body
 	def printArticleBody(self):
 		for body in self.articleBody:
 			print(body + "\n")
 
+	# scrape and store article title and body. Save article body as array of paragraphs
 	def scrape(self):
 		page = requests.get(self.articleURL)
 		soup_page = soup(page.content, 'html.parser')
-
 		self.articleTitle = soup_page.find_all(class_="util-bar-share-summary-title")[0].get_text()
-
 		body = soup_page.find_all(class_="p-text")
 
 		for paragraph in body:
@@ -80,8 +97,8 @@ class FreepScraper:
 scraper_test = FreepScraper("https://www.freep.com/story/news/local/michigan/" + 
 	"detroit/2019/12/05/detroit-bulk-storage-revere-copper-detroit-river-uranium/2618868001/")
 
-spider_test = FreepCrawler("pollution")
-print(spider_test.getURLs())
+# spider_test = FreepCrawler("pollution")
+# print(spider_test.crawlURLs())
 
 
 
