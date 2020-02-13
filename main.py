fa129bb6848fcaee7e4f333379805ec9a249253f
Saular Raffi <fo6197@wayne.edu>
2020-02-13 11:52:21 -0500

    null

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from newsWebsite import NewsWebsite
 from crawler import Crawler
 from scraper import Scraper
 
-stignacenews = NewsWebsite("https://www.stignacenews.com", "", "", "", "/articles/", False)
+stignacenews = NewsWebsite("https://www.stignacenews.com", "title", "", "", "/articles/", False)
 # crawler = Crawler(stignacenews, "pollution", "contamination")
 #
 # for url in crawler.getCrawledURLs():
 #     print(url)
 
-ourmidland = NewsWebsite("https://www.ourmidland.com", "", "", "", "/news/article/", False)
+ourmidland = NewsWebsite("https://www.ourmidland.com", "title", "meta", "time", "/news/article/", False)
 # crawler = Crawler(ourmidland, "pollution", "contamination")
 #
 # for url in crawler.getCrawledURLs():
@@ -16,4 +16,4 @@ ourmidland = NewsWebsite("https://www.ourmidland.com", "", "", "", "/news/articl
 
 websiteList = [stignacenews,ourmidland]
 
-scraper = Scraper("https://www.stignacenews.com/articles/peters-named-among-most-effective-lawmakers/", websiteList)
+scraper1 = Scraper("https://www.ourmidland.com/news/article/Environmentalists-challenge-DNR-sand-mine-8373534.php", websiteList)
--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup as soup
 
 ################################################
 #                Scraper Class                 #
@@ -7,6 +9,9 @@ class Scraper:
     def __init__(self, url, websiteObjList):
         self.articleURL = url
         self.websiteObjList = websiteObjList
+        self.articleTitle = ""
+        self.articleBody = []
+        self.articleDate = ""
 
         websiteName = url.split("www.")[1].split(".com")[0]
 
@@ -17,5 +22,23 @@ class Scraper:
 
     def scrape(self):
         print("[+] Scraping " + self.articleURL)
+        page = requests.get(self.articleURL)
+        soup_page = soup(page.content, 'html.parser')
+
+        # scrape article title
+        self.articleTitle = soup_page.find_all(self.website.getTitleTag())[0].get_text()
+
+        # scape article body
+        body = soup_page.find_all(self.website.getBodyTag())
+
+        for line in body:
+            self.articleBody.append(line)
+
+        # scrape article publishing date
+        date = soup_page.find_all(self.website.getPublishingDateTag())[0].get_text().strip()
+
+        print(date)
+
+
 
 
