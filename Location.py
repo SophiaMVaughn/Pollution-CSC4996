18c6a42f1996e32c1ea393c62e381b805c5d2292
SophiaMVaughn <cq7614@wayne.edu>
2020-03-01 16:10:37 -0500

    Fixed some location bugs

--- Location.py
+++ Location.py
@@ -3,7 +3,7 @@ from officialComm import convertScrapedtoSent
 
 def locationsInfo(articleBody):
     # reading from a body of text to find locations
-    test = open("LocationTest.txt", "r")
+    #test = open("LocationTest.txt", "r")
     lakes = re.compile(r'(?i) *lake (\w+)')
     rivers = re.compile(r'(?i)\w+(?=\s+river)')
     #store all locations in array
@@ -12,10 +12,10 @@ def locationsInfo(articleBody):
     for para in articleBody:
         temp= convertScrapedtoSent(para)
         for sent in temp:
-            lake=lakes.finall(sent)
+            lake=lakes.findall(sent)
             for Lake in lake:
                 local.append(Lake)
             river =rivers.findall(sent)
             for River in river:
-                local.append(Lake)
+                local.append(River)
     return local
