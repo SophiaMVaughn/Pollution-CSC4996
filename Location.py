d207954f91513525d4756f2da44c336df877fb3b
fv7793 <MoogleToast1!>
2020-03-01 17:27:01 -0500

    Fixed the issue with periods

--- Location.py
+++ Location.py
@@ -4,8 +4,9 @@ from officialComm import convertScrapedtoSent
 def locationsInfo(articleBody):
     # reading from a body of text to find locations
     #test = open("LocationTest.txt", "r")
-    lakes = re.compile(r'(?i)\S*lake\S*(?:\s\S+)?')
+    lakes = re.compile(r'(?i)\S*lake\S*(?:\s([a-zA-Z]+))?')
     rivers = re.compile(r'(?i)(?:\S+\s)?\S*river')
+
     #store all locations in array
     local = []
     # return all locations found in body of text from file
