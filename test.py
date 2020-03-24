3612124b1ba3701b443f9171a1d58d155b4d8436
Saular Raffi <fo6197@wayne.edu>
2020-03-24 18:50:27 -0400

    added firefox driver and linux chrome driver

--- test.py
+++ test.py
@@ -13,7 +13,8 @@ import sys
 import datetime
 from selenium import webdriver
 from sys import platform
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 import os
 from website import Website
 from crawler import Crawler
@@ -22,16 +23,10 @@ from newspaper import urls as urlChecker
 from newspaper import Article
 from dateutil import parser
 
-website = Website("https://www.record-eagle.com/")
-website.searchForKey("pollution")
-
-driver = website.getDriver()
-
-articles = driver.find_elements_by_class_name("gs-title")
-for article in articles:
-    print(article)
-    try:
-        link = article.find_element_by_css_selector('a').get_attribute('href')
-        print(link)
-    except:
-        pass
+# firefoxDriverPath = os.path.abspath(os.getcwd())
+chromeDriverPath = os.path.abspath(os.getcwd()) + "/chromedriver_79_mac"
+options = Options()
+# options.add_argument('--headless')
+driver = webdriver.Chrome(chromeDriverPath, options=options)
+# driver = webdriver.Firefox(firefoxDriverPath, options=options)
+driver.get("https://www.mlive.com/search/?q=pollution")
