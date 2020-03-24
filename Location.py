dddab9c8852a475e9ef7ed058d58740227a4a1a8
fv7793 <MoogleToast1!>
2020-03-24 19:13:44 -0400

    Improved performance

--- Location.py
+++ Location.py
@@ -7,18 +7,9 @@ def locationsInfo(articleBody):
     # test = open("LocationTest.txt", "r")
     lakes = re.compile(r'\S*Lake\S*(?:\s([A-Z]+))\S*')
     rivers = re.compile(r'(?:(([A-Z]))\S+\s)?\S*River')
-    countys = re.compile(r'(?:(([A-Z]))\S+\s)?\S*County')
-    creeks = re.compile(r'(?:(([A-Z]))\S+\s)?\S*Creek')
-    townships = re.compile(r'(?:(([A-Z]))\S+\s)?\S*Townships')
-    parks = re.compile(r'(?:(([A-Z]))\S+\s)?\S*Parks')
     schools = re.compile(r'\S*Elementary School|Intermediate School|Middle School|High School')
     highways = re.compile(r'I-(?:\S+\s)?|M-(?:\S+\s)?')
-    powerplants = re.compile(r'(?:(([A-Z]))\S+\s)?\S*Power Plant')
-    powerstations = re.compile(r'(?:(([A-Z]))\S+\s)?\S*Power Station')
-    bays = re.compile(r'(?:(([A-Z]))\S+\s)?\S*Bay')
-    ponds = re.compile(r'(?:(([A-Z]))\S+\s)?\S*Pond')
-    dams = re.compile(r'(?:(([A-Z]))\S+\s)?\S*Dam')
-    deltas = re.compile(r'(?:(([A-Z]))\S+\s)?\S*Delta')
+    wordBefore = re.compile(r'(?:(([A-Z]))\S+\s)?\S*(Power Plant|Power Station|Bay|Pond|Dam|Delta|Parks|Townships|Creek|County|River)')
     cityLoc = re.compile(r'^[A-Z]\w*')
     cities = []
     cityFile = open("cities.txt", "r")
@@ -30,29 +21,9 @@ def locationsInfo(articleBody):
     for para in articleBody:
         temp = convertScrapedtoSent(para)
         for sent in temp:
-            delta = deltas.findall(sent)
-            for Delta in delta:
-                local.append(Delta)
-                break
-            pond = ponds.findall(sent)
-            for Pond in pond:
-                local.append(Pond)
-                break
-            dam = dams.findall(sent)
-            for Dam in dam:
-                local.append(Dam)
-                break
-            bay = bays.findall(sent)
-            for Bay in bay:
-                local.append(Bay)
-                break
-            powerplant = powerplants.findall(sent)
-            for PowerPlant in powerplant:
-                local.append(PowerPlant)
-                break
-            powerstation = powerstations.findall(sent)
-            for PowerStation in powerstation:
-                local.append(PowerStation)
+            wordsBefore = wordBefore.findall(sent)
+            for wordbefore in wordsBefore:
+                local.append(wordbefore)
                 break
             lake = lakes.findall(sent)
             for Lake in lake:
@@ -62,22 +33,6 @@ def locationsInfo(articleBody):
             for River in river:
                 local.append(River)
                 break
-            creek = creeks.findall(sent)
-            for Creek in creek:
-                local.append(Creek)
-                break
-            county = countys.findall(sent)
-            for County in county:
-                local.append(County)
-                break
-            township = townships.findall(sent)
-            for Township in township:
-                local.append(Township)
-                break
-            park = parks.findall(sent)
-            for Park in park:
-                local.append(Park)
-                break
             school = schools.findall(sent)
             for School in school:
                 local.append(School)
@@ -86,7 +41,6 @@ def locationsInfo(articleBody):
             for Highway in highway:
                 local.append(Highway)
                 break
-
     # cities calculation (ONly returns the first city found for right now)
     cities = []
     cityFile = open("Cities.txt", "r")
